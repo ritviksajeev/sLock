80a9079beab3c520a187da0d17727cea93609a1a
ev <evzero@icloud.com>
2025-02-09 15:21:59 +0530

    v0.1
    Skeletal structure of the website

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Display Date and Time
+function updateDateTime() {
+    const now = new Date();
+    const datetimeElement = document.getElementById('datetime');
+    datetimeElement.textContent = now.toLocaleString();
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime(); // Initial call
+
+// Mobile Device Detection
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobileDevice()) {
+    alert("Please visit this site on a PC for the best experience.");
+    document.body.innerHTML = "<h1 style='text-align: center; margin-top: 50vh;'>Please use a PC to access this website.</h1>";
+}
